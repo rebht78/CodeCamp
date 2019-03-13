f9b82327cdae835ceba35f092b43674c0f594600
Sohel Shaikh <rebht78@gmail.com>
2019-03-13 21:55:17 +0530

    added standard routes and third party dependencies

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var app = express();
+app.set("view engine","ejs");
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.listen("8080", function() {
+    console.log("CodeCamp started successfully!!!");
+});
