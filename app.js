5620916ddcea7d96bd9742650c0b63e00592d182
Sohel Shaikh <rebht78@gmail.com>
2019-03-18 22:13:41 +0530

    changes to login, regiser and footer. also added gitignore

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from "express";
+var express = require('express');
 
 var app = express();
 app.set("view engine","ejs");
@@ -9,6 +9,10 @@ app.get("/", function(req, res){
 
 app.get("/login", function(req, res){
     res.render("login");
+});
+
+app.post("/login", function(req, res) {
+    
 });
 
 app.get("/register", function(req, res){
