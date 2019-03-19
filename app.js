231757d41060f022ce837246b79b670d94973624
Sohel Shaikh <rebht78@gmail.com>
2019-03-19 22:10:30 +0530

    added support for static resources.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
 app.set("view engine","ejs");
 
 app.get("/", function(req, res){
