d59951523d459852de2850270b7e109c90158787
Sohel Shaikh <rebht78@gmail.com>
2019-03-25 22:16:28 +0530

    added AddUser script for inserting user

--- public/js/codecamp.js
+++ public/js/codecamp.js
@@ -8,6 +8,25 @@ function RegisterLoader()
     HideMainDiv();
     ajaxCallGet("/register",'load');
 }
+function AddUser()
+{
+    if (validate_compare())
+    {
+        var formdata = new FormData();
+        formdata.append("Username",document.getElementById('txtUsername').value);
+        formdata.append("Password",document.getElementById('txtPassword').value);
+        ajaxCallPost("/register",formdata,'load');
+        
+        if (document.getElementById('hiddenstatus').value > 1)
+        {
+            alert("You have been registered successfully!");
+        }
+        else
+        {
+            alert("Problem in adding your data, try again later!");
+        }
+    }
+}
 function HideMainDiv()
 {
     document.getElementById('maindiv').style.display = "none";
